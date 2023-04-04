227c3e10fe38359294a1f29b7e94015f28f4437e
meetawaisali <awais.ali@tintash.com>
2023-04-05 02:38:53 +0500

    Feature - Implemented UI for signup and login with firebase

---
+++ BlogApp/App.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable react/react-in-jsx-scope */
+import React from 'react';
+import Login from './Screens/Login';
+
+const App = () => {
+  return <Login />;
+};
+
+export default App;
